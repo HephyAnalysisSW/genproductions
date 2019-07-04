54eaa295313d9c16ae2685ff442b1f2a59071b82
schoef <robert.schoefbeck@cern.ch>
2019-07-04 04:51:03 -0500

    small

--- bin/MadGraph5_aMCatNLO/make_reweight_card.sh
+++ bin/MadGraph5_aMCatNLO/make_reweight_card.sh
@@ -1,2 +1,2 @@
 cardname="ttGamma_CMS"
-python make_reweight_card.py --filename addons/cards/${cardname}/${cardname}_reweight_card.dat --overwrite --reference ctZ 4 ctZI 4 ctW 4 ctWI 4 ctG 4 ctGI 4 --couplings 4 cpt 1 cpQM 1 ctZ 1 ctZI 1 ctW 1 ctWI 1 ctG 1 ctGI 1
+python make_reweight_card.py --filename addons/cards/${cardname}/${cardname}_reweight_card.dat --overwrite --reference ctZ 4 ctZI 4 --couplings 4 cpt 1 cpQM 1 ctZ 1 ctZI 1
